af51c240d3e3c58352f619ac3f895cf7676ee199
clussier <lussierc@allegheny.edu>
2018-11-13 14:20:49 -0500

    Created the 'src' file where R code for the project will be held. Put a file with R code inside this file.

---
+++ src/analysisCode.R
@@ -0,0 +1,3 @@
+appTable2_2016_FoodBorneOutbreaks_data <- read.csv("~/cs301F2018/cs301f2018-project-starter-townies/data/appTable2_2016_FoodBorneOutbreaks_data.csv", header=FALSE)
+View(appTable2_2016_FoodBorneOutbreaks_data)
+
