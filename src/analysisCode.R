64c0102c624c7b65d658de89e7edacb997c2e3c4
clussier <lussierc@allegheny.edu>
2018-11-13 14:33:49 -0500

    Added R code that contains imports/objects for all data tables in 2016 Foodborne Outbreaks Data.

--- src/analysisCode.R
+++ src/analysisCode.R
@@ -1,3 +1,46 @@
+## CS301 - Final Project Code
+# Group Members: Christian Lussier, Dillon Thoma, Nick Tocci, Ben Watto
+
+# --- Import Data From 2016 Foodborne Outbreaks --- #
+# - Import Regular Tables From 2016 Foodborne Outbreaks - #
+table1_2016_FoodBorneOutbreaks <- read.csv("~/cs301F2018/cs301f2018-project-starter-townies/data/table1_2016_FoodBorneOutbreaks.csv")
+View(table1_2016_FoodBorneOutbreaks)
+
+table2a_2016_FoodBorneOutbreaks <- read.csv("~/cs301F2018/cs301f2018-project-starter-townies/data/table2_2016_FoodBorneOutbreaks.csv")
+View(table2_2016_FoodBorneOutbreaks)
+
+table2b_2016_FoodBorneOutbreaks <- read.csv("~/cs301F2018/cs301f2018-project-starter-townies/data/table2b_2016_FoodBorneOutbreaks.csv")
+View(table2b_2016_FoodBorneOutbreaks)
+
+table3a_2016_FoodBorneOutbreaks <- read.csv("~/cs301F2018/cs301f2018-project-starter-townies/data/table3a_2016_FoodBorneOutbreaks.csv")
+View(table3a_2016_FoodBorneOutbreaks)
+
+table3b_2016_FoodBorneOutbreaks <- read.csv("~/cs301F2018/cs301f2018-project-starter-townies/data/table3b_2016_FoodBorneOutbreaks.csv")
+View(table3b_2016_FoodBorneOutbreaks)
+
+table3c_2016_FoodBorneOutbreaks <- read.csv("~/cs301F2018/cs301f2018-project-starter-townies/data/table3c_2016_FoodBorneOutbreaks.csv")
+View(table3c_2016_FoodBorneOutbreaks)
+
+table4_2016_FoodBorneOutbreaks <- read.csv("~/cs301F2018/cs301f2018-project-starter-townies/data/table4_2016_FoodBorneOutbreaks.csv")
+View(table4_2016_FoodBorneOutbreaks)
+
+# - Import Appendix Tables From 2016 Foodborne Outbreaks - #
+appTable1_2016_FoodBorneOutbreaks_data <- read.csv("~/cs301F2018/cs301f2018-project-starter-townies/data/appTable1_2016_FoodBorneOutbreaks_data.csv", header=FALSE)
+View(appTable1_2016_FoodBorneOutbreaks_data)
+
 appTable2_2016_FoodBorneOutbreaks_data <- read.csv("~/cs301F2018/cs301f2018-project-starter-townies/data/appTable2_2016_FoodBorneOutbreaks_data.csv", header=FALSE)
 View(appTable2_2016_FoodBorneOutbreaks_data)
+
+appTable3_2016_FoodBorneOutbreaks_data <- read.csv("~/cs301F2018/cs301f2018-project-starter-townies/data/appTable3_2016_FoodBorneOutbreaks_data.csv", header=FALSE)
+View(appTable3_2016_FoodBorneOutbreaks_data)
+
+appTable4_2016_FoodBorneOutbreaks_data <- read.csv("~/cs301F2018/cs301f2018-project-starter-townies/data/appTable4_2016_FoodBorneOutbreaks_data.csv", header=FALSE)
+View(appTable4_2016_FoodBorneOutbreaks_data)
+
+appTable5_2016_FoodBorneOutbreaks_data <- read.csv("~/cs301F2018/cs301f2018-project-starter-townies/data/appTable5_2016_FoodBorneOutbreaks_data.csv", header=FALSE)
+View(appTable5_2016_FoodBorneOutbreaks_data)
+
+appTable6_2016_FoodBorneOutbreaks_data <- read.csv("~/cs301F2018/cs301f2018-project-starter-townies/data/appTable6_2016_FoodBorneOutbreaks_data.csv", header=FALSE)
+View(appTable6_2016_FoodBorneOutbreaks_data)
+
 
