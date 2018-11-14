e07a7e469676d659471f4c92770211715164bb21
clussier <lussierc@allegheny.edu>
2018-11-14 18:38:14 -0500

    Adding recent writing to project progress report.

--- src/analysisCode.R
+++ src/analysisCode.R
@@ -10,9 +10,9 @@ library(tidyverse)
 # --- Import Data From 2016 Foodborne Outbreaks --- #
 # - Import Regular Tables From 2016 Foodborne Outbreaks - #
 table1_2016_FoodBorneOutbreaks <- read.csv("~/cs301F2018/cs301f2018-project-starter-townies/data/table1_2016_FoodBorneOutbreaks_data.csv")
-View(table1_2016_FoodBorneOutbreaks) # Shows the data for the object
 
 table2a_2016_FoodBorneOutbreaks <- read.csv("~/cs301F2018/cs301f2018-project-starter-townies/data/table2_2016_FoodBorneOutbreaks_data.csv")
+View(table2a_2016_FoodBorneOutbreaks) # Shows the data for the object
 
 table2b_2016_FoodBorneOutbreaks <- read.csv("~/cs301F2018/cs301f2018-project-starter-townies/data/table2b_2016_FoodBorneOutbreaks_data.csv")
 
@@ -39,6 +39,19 @@ appTable6_2016_FoodBorneOutbreaks_data <- read.csv("~/cs301F2018/cs301f2018-proj
 # --- End Data Imports --- #
 
 
+# ----- PLOTTING -----:
+# BY ETIOLOGY: 
+# Plots scatterplot that compares the outbreaks by confirmed etiology and suspected etiology. 
 ggplot(data = table1_2016_FoodBorneOutbreaks) + geom_point(mapping = aes(x = No..Outbreaks.CE, y = SE, color = Etiology), position = "dodge", stat = "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=-0.01))
 
+# Plots bar graph that shows the numbers of confirmed outbreaks by etiology.
 ggplot(data = table1_2016_FoodBorneOutbreaks) + geom_bar(mapping = aes(x = Etiology, y = No..Outbreaks.CE), position = "dodge", stat = "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=-0.01))
+
+# Plots bar graph that shows the numbers of confirmed illnesses by etiology.
+ggplot(data = table1_2016_FoodBorneOutbreaks) + geom_bar(mapping = aes(x = Etiology, y = No..Illnesses.CE), position = "dodge", stat = "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=-0.01))
+
+# Plots bar graph that shows the numbers of confirmed hospitalizations by etiology.
+ggplot(data = table1_2016_FoodBorneOutbreaks) + geom_bar(mapping = aes(x = Etiology, y = No..Hospitalizations.CE), position = "dodge", stat = "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=-0.01))
+
+# Plots bar graph that shows the numbers of confirmed hospitalizations by etiology.
+ggplot(data = table1_2016_FoodBorneOutbreaks) + geom_histo(mapping = aes(x = Food.Category., y = No..Outbreaks.Total), position = "dodge", stat = "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=-0.01))
